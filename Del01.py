392ce5ba56d0f1238f591cf367e49f004cd6c4d7
JJ Thissell <chief@JJs-MacBook-Pro.local>
2019-08-29 22:37:26 -0400

    made it so hands could be detected and Handle_Frame function prints the hand id of the first hand.

--- Del01.py
+++ Del01.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, '..')
+import Leap
 ##from pygameWindow import PYGAME_WINDOW
 #import random
 #import constants as constants
@@ -25,12 +28,26 @@
 #        y = y+constants.circleVelocity
 
 #print(pygameWindow)
-#while True:
+controller = Leap.Controller()
+
+def Handle_Frame(frame):
+    hand = frame.hands[0]
+    print(hand)
+
+while True:
 #    pygameWindow.Prepare()
 #    pygameWindow.Draw_Black_Circle(x, y)
 #    Perturb_Circle_Position()
 #    pygameWindow.Reveal()
+    frame = controller.frame()
 
-import sys
-sys.path.insert(0, '..')
-import Leap
+    ##want to change the position of the dot only when you hover
+    ##your hand over the device
+    #hands = frame.hands[0]
+    handlist = frame.hands
+    for hands in handlist:
+        #if the list is not empty
+        if(handlist > 0):
+            Handle_Frame(frame)
+            #print("Hand detected")
+            #print(len(handlist))
