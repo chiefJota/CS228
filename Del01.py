e4fe43da14bbf2ca1d9a4d6b19729b822bd0d7b6
JJ Thissell <chief@JJs-MacBook-Pro.local>
2019-08-29 20:05:33 -0400

    testing changes

--- Del01.py
+++ Del01.py
@@ -1,32 +1,36 @@
-from pygameWindow import PYGAME_WINDOW
-import random
-import constants as constants
+##from pygameWindow import PYGAME_WINDOW
+#import random
+#import constants as constants
 
-pygameWindow = PYGAME_WINDOW()
+#pygameWindow = PYGAME_WINDOW()
 
-x = 450
-y = 450
+#x = 450
+#y = 450
 
-def Perturb_Circle_Position():
-    global x, y
-    fourSidedDieRoll = random.randint(1,4)
+#def Perturb_Circle_Position():
+#    global x, y
+#    fourSidedDieRoll = random.randint(1,4)
 
     #decrease horizontal position by 1
-    if(fourSidedDieRoll == 1):
-        x = x-constants.circleVelocity
+#    if(fourSidedDieRoll == 1):
+#        x = x-constants.circleVelocity
     #increase horizontal position by 1
-    elif(fourSidedDieRoll == 2):
-        x = x+constants.circleVelocity
+#    elif(fourSidedDieRoll == 2):
+#        x = x+constants.circleVelocity
     #decrease vertical position by 1
-    elif(fourSidedDieRoll == 3):
-        y = y-constants.circleVelocity
+#    elif(fourSidedDieRoll == 3):
+#        y = y-constants.circleVelocity
     #increase vertical position by 1
-    else:
-        y = y+constants.circleVelocity
+#    else:
+#        y = y+constants.circleVelocity
 
 #print(pygameWindow)
-while True:
-    pygameWindow.Prepare()
-    pygameWindow.Draw_Black_Circle(x, y)
-    Perturb_Circle_Position()
-    pygameWindow.Reveal()
+#while True:
+#    pygameWindow.Prepare()
+#    pygameWindow.Draw_Black_Circle(x, y)
+#    Perturb_Circle_Position()
+#    pygameWindow.Reveal()
+
+import sys
+sys.path.insert(0, '..')
+import Leap
